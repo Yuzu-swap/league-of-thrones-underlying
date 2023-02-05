ac0d2a15bafb9c046cc1c34a16542ecf26f18f4f
fakeboom <fakeboom@foxmail.com>
2023-02-05 15:14:14 +0800

    update gds

--- src/Game/Throne/index.ts
+++ src/Game/Throne/index.ts
@@ -805,7 +805,7 @@ export class Throne implements IThrone {
   constructor() {
     this.inited = false
     this.instanceState = InstanceStatus.Null
-    this.version = "u20230118"
+    this.version = "u20230205"
   }
 
 
