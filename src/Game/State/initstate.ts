5712b55ab2e0e27f38c35cc1a440d38b79e98d09
fakeboom <fakeboom@foxmail.com>
2023-02-06 23:40:19 +0800

    change init troop

--- src/Game/State/initstate.ts
+++ src/Game/State/initstate.ts
@@ -18,7 +18,7 @@ export var InitState = {
           [ResouceType.Troop]:
           {
               lastUpdate: -1,
-              value: 1000000,
+              value: 1000,
           }
       },
       recruit:[],
