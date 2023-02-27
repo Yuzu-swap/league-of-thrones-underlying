9c221784cee33295b2002ce0b3b6c4666034c953
fakeboom <fakeboom@foxmail.com>
2023-02-28 00:37:40 +0800

    change version

--- src/Game/Throne/index.ts
+++ src/Game/Throne/index.ts
@@ -999,7 +999,7 @@ export class Throne implements IThrone {
   constructor() {
     this.inited = false
     this.instanceState = InstanceStatus.Null
-    this.version = "u2023022601"
+    this.version = "u2023022801"
   }
 
 
