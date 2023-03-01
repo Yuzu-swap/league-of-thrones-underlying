eaffce2eb5ed0c365a168bcfd8bd72585b1a8d91
fakeboom <fakeboom@foxmail.com>
2023-03-01 23:12:16 +0800

    temp commit

--- src/Game/State/index.ts
+++ src/Game/State/index.ts
@@ -113,6 +113,7 @@ export interface IRewardGlobalState extends IState{
   contractAddressInput: string[]
   contractGloryInput: number[]
   unionGlorySum: number
+  unionGlorySumRuntime: number[] 
   unionWinId: number
   seasonEnd: boolean
   unionRewardResult: RewardResult[]
