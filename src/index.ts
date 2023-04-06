e13217373facec491135eb182fb28d97a7f54e58
水色 <shuiseshuise@hotmail.com>
2023-04-06 22:12:40 +0800

    randomCampReward as static prop

--- src/index.ts
+++ src/index.ts
@@ -39,6 +39,9 @@ import { addToNormalSortedList, addToSortList, checkNaNInObj, getTimeStamp, isNu
 import { StrategyComponent } from './Game/Throne/strategy';
 import { ChainComponent, IChainComponent } from './Game/Throne/chain';
 
+import parameterGDS = require('./league-of-thrones-data-sheets/.jsonoutput/parameter.json')
+
+export const randomCampReward = parameterGDS.choose_random_camp_reward;
 export const GameName = 'league of thrones';
 export * from './Game/Controler/mediator';
 export * from './Game/Controler/transition';
