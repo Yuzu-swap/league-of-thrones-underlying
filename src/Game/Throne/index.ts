64ab9ef2f830ce7e9c7b723a8eae81a21a238358
水色 <shuiseshuise@hotmail.com>
2023-08-16 23:59:12 +0800

    new version

--- src/Game/Throne/index.ts
+++ src/Game/Throne/index.ts
@@ -1173,7 +1173,7 @@ export class Throne implements IThrone {
   constructor() {
     this.inited = false
     this.instanceState = InstanceStatus.Null
-    this.version = "u0808"
+    this.version = "u816"
   }
 
 
