db6b5d51dd90a6a9069e56883de008ba9a2e73b5
水色 <shuiseshuise@hotmail.com>
2023-08-22 21:58:01 +0800

    new assembly gds

--- src/Game/Throne/index.ts
+++ src/Game/Throne/index.ts
@@ -1173,7 +1173,7 @@ export class Throne implements IThrone {
   constructor() {
     this.inited = false
     this.instanceState = InstanceStatus.Null
-    this.version = "u818"
+    this.version = "u822"
   }
 
 
